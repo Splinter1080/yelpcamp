b63f43a5dcbb55cecd639ae127fc6a784fb53aa9
Splinter1080 <supreetjoshi2k15@gmail.com>
2020-10-27 17:38:13 +0530

    added cluster

--- controllers/camogrounds.js
+++ controllers/camogrounds.js
@@ -7,7 +7,7 @@ const geocoder = mbxGeocoding({ accessToken : mapBoxToken});
 
 
 module.exports.index = async (req, res) => {
-    const campgrounds = await Campground.find({});
+    const campgrounds = await Campground.find({}).populate('popupText');
     res.render('campgrounds/index', { campgrounds })
 }
 
